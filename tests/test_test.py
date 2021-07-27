845ae8b0741aebaffc66e615d7cd509c5cbf596e
onerandomusername <genericusername414+git@gmail.com>
2021-07-27 16:20:47 -0400

    add dev dependencies, update tasks list
    add deps:
    - pytest
    - pytest-cov
    - pytest-xdist
    - codecov
    - coverage.py
    
    update tasks list so all tasks have a description of what they do

---
+++ tests/test_test.py
@@ -0,0 +1,13 @@
+import unittest
+
+"""
+To ensure workflows succeed, this is a temporary test file for github actions.
+"""
+
+
+class TestTest(unittest.TestCase):
+    """Demo test"""
+
+    def test_test(self):
+        """True is true"""
+        self.assertTrue(True)
