ff75614c8eca3988a37431fd492f6a1712ed2666
onerandomusername <genericusername414+git@gmail.com>
2021-08-01 00:23:19 -0400

    chore: Remove website
    - remove all website components.
    - Closes #32
    
    Signed-off-by: onerandomusername <genericusername414+git@gmail.com>

--- website/constants.py
+++
@@ -1,9 +0,0 @@
-from os import environ
-
-import dotenv
-
-dotenv.load_dotenv()
-
-REDIRECT_URI = environ.get("REDIRECT_URI")
-DISCORD_CLIENT_ID = environ.get("CLIENT_ID")
-DISCORD_CLIENT_SECRET = environ.get("CLIENT_SECRET")
