5b11fb485ac0d9a3571ee9157913435fcf3a1e2f
onerandomusername <genericusername414+git@gmail.com>
2021-08-30 01:08:18 -0400

    minor: don't use the global ext/plug lists
    Signed-off-by: onerandomusername <genericusername414+git@gmail.com>

--- tests/modmail/extensions/test_extension_manager.py
+++ tests/modmail/extensions/test_extension_manager.py
@@ -3,16 +3,19 @@ from copy import copy
 import pytest
 
 from modmail.extensions.extension_manager import ExtensionConverter
-from modmail.utils.extensions import EXTENSIONS, walk_extensions
+from modmail.utils.extensions import EXTENSIONS as GLOBAL_EXTENSIONS
+from modmail.utils.extensions import walk_extensions
 
 
 # load EXTENSIONS
-EXTENSIONS = copy(EXTENSIONS)
+EXTENSIONS = copy(GLOBAL_EXTENSIONS)
 EXTENSIONS.update(walk_extensions())
 
 
 class TestExtensionConverter:
     """Test the extension converter converts extensions properly."""
+
+    all_extensions = {x: y for x, y in walk_extensions()}
 
     @pytest.fixture(scope="class", name="converter")
     def converter(self) -> ExtensionConverter:
@@ -20,10 +23,10 @@ class TestExtensionConverter:
         return ExtensionConverter()
 
     @pytest.mark.asyncio
-    @pytest.mark.parametrize("extension", [e.rsplit(".", 1)[-1] for e in EXTENSIONS.keys()])
+    @pytest.mark.parametrize("extension", [e.rsplit(".", 1)[-1] for e in all_extensions.keys()])
     async def test_conversion_success(self, extension: str, converter: ExtensionConverter) -> None:
         """Test all extensions in the list are properly converted."""
-        converter.source_list = EXTENSIONS
+        converter.source_list = self.all_extensions
         converted = await converter.convert(None, extension)
 
         assert converted.endswith(extension)
--- tests/modmail/extensions/test_plugin_manager.py
+++ tests/modmail/extensions/test_plugin_manager.py
@@ -3,16 +3,19 @@ from copy import copy
 import pytest
 
 from modmail.extensions.plugin_manager import PluginConverter
-from modmail.utils.plugins import PLUGINS, walk_plugins
+from modmail.utils.plugins import PLUGINS as GLOBAL_PLUGINS
+from modmail.utils.plugins import walk_plugins
 
 
 # load EXTENSIONS
-PLUGINS = copy(PLUGINS)
+PLUGINS = copy(GLOBAL_PLUGINS)
 PLUGINS.update(walk_plugins())
 
 
 class TestExtensionConverter:
     """Test the extension converter converts extensions properly."""
+
+    all_plugins = {x: y for x, y in walk_plugins()}
 
     @pytest.fixture(scope="class", name="converter")
     def converter(self) -> PluginConverter:
@@ -20,10 +23,10 @@ class TestExtensionConverter:
         return PluginConverter()
 
     @pytest.mark.asyncio
-    @pytest.mark.parametrize("extension", [e.rsplit(".", 1)[-1] for e in PLUGINS.keys()])
+    @pytest.mark.parametrize("extension", [e.rsplit(".", 1)[-1] for e in all_plugins.keys()])
     async def test_conversion_success(self, extension: str, converter: PluginConverter) -> None:
         """Test all extensions in the list are properly converted."""
-        converter.source_list = PLUGINS
+        converter.source_list = self.all_plugins
         converted = await converter.convert(None, extension)
 
         assert converted.endswith(extension)
