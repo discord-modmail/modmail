74a25d5cf182bcc9386350d2c8fb4ef84fd99da6
Bast <bast@fastmail.com>
2021-08-31 23:16:54 -0700

    Add basic event dispatcher

---
+++ modmail/utils/dispatcher.py
@@ -0,0 +1,159 @@
+import asyncio
+import bisect
+import logging
+from typing import Callable, Coroutine, Dict, List, Optional, Union
+
+from modmail import ModmailLogger
+
+logger: ModmailLogger = logging.getLogger("modmail.dispatcher")
+
+CoroutineFunction = Callable[..., Coroutine]
+
+
+class Dispatcher:
+    """
+    Dispatches events through an async event handler system.
+
+    Supports blocking events and priority.
+    """
+
+    # These are separate because it makes using the bisect module easier. See _register_handler.
+    blocking_handlers: Dict[str, List[Optional[CoroutineFunction]]]
+    blocking_priorities: Dict[str, List[int]]
+    handlers: Dict[str, List[CoroutineFunction]]
+
+    def __init__(self, *event_names: str):
+        self.handlers = {}
+        self.blocking_handlers = {}
+        self.blocking_priorities = {}
+
+        self.register_event(*event_names)
+
+    def register_event(self, *event_names: str) -> None:
+        """
+        Registers an/some event type(s).
+
+        If there's a problem/typo then registering handlers to unknown event types, or
+        calling handlers on an unknown event type will trigger a warning message.
+        """
+        for event_name in event_names:
+            self.handlers[event_name] = []
+            self.blocking_handlers[event_name] = []
+            self.blocking_priorities[event_name] = []
+
+    def _register_handler(
+        self,
+        event_name: Optional[str],
+        priority: Optional[int],
+        func: CoroutineFunction,
+    ) -> None:
+        """
+        Actually register the handler.
+
+        This function contains registration code, to keep it separate from the decorator code.
+        """
+        logger.debug("Registering %r to be called for event '%s', priority %s", func, event_name, priority)
+
+        # This better fits under register(), but it'd be duped there.
+        # This extracts the event name from the function name, formatted on_event_name, if it's not provided
+        if not event_name:
+            if func.__name__.startswith("on_"):
+                event_name = func.__name__[3:]
+            else:
+                raise ValueError(
+                    "You must pass an event name if the function name doesn't follow the on_eventname format."
+                )
+
+        if event_name not in self.handlers:
+            logger.warning(
+                "Warning: event handler %r registered for event name '%s' that was not registered.",
+                func,
+                event_name,
+            )
+            self.handlers[event_name] = []
+            self.blocking_handlers[event_name] = []
+            self.blocking_priorities[event_name] = []
+
+        if priority is None:
+            self.handlers[event_name].append(func)
+        else:
+            index = bisect.bisect_left(self.blocking_priorities[event_name], priority)
+            self.blocking_priorities[event_name].insert(index, priority)
+            self.blocking_handlers[event_name].insert(index, func)
+
+    def register(
+        self,
+        event_name: Optional[str] = None,
+        func: Optional[CoroutineFunction] = None,
+        priority: Optional[int] = None,
+    ) -> Union[CoroutineFunction, Callable]:
+        """
+        Register an event handler to be called when this event is dispatched.
+
+        This can be used both as a raw call (`register("thread_create", priority=3)`)
+         or as a decorator:
+         @dispatcher.register("thread_create")
+         def handle_new_threads(...
+
+         If the event name is not provided, it is extracted from the method name similar to how dpy does it.
+
+        Priority is optional. If provided, the handler will be called in order according to it's priority.
+        If the handler returns True, the event is considered "handled" and further dispatch is cancelled.
+        Lower numbers go first. Negative numbers are supported.
+
+        If priority is not provided the event is dispatched asynchronously after all blocking handlers,
+         and the return result of the handler has no effect.
+        """
+        if func:
+            self._register_handler(event_name, priority, func)
+            return func
+
+        def register_decorator(func: CoroutineFunction) -> CoroutineFunction:
+            self._register_handler(event_name, priority, func)
+            return func
+
+        return register_decorator
+
+    def unregister(self, func: CoroutineFunction, *event_names: Optional[str]) -> None:
+        """
+        Unregister a function from dispatch.
+
+        If event_names is not provided, removes it from all handlers.
+        If you have accidentally added a handler twice to an event, this will only remove one instance.
+        """
+        if not event_names:
+            event_names = self.handlers.keys()
+
+        for event_name in event_names:
+            if func in self.handlers[event_name]:
+                self.handlers[event_name].remove(func)
+
+            if func in self.blocking_handlers[event_name]:
+                # blocking handlers are two separate lists because it makes searching for items
+                # (and thus bisect) a hundred times easier. But that does mean we have to keep them in sync.
+                index = self.blocking_handlers[event_name].index(func)
+                del self.blocking_handlers[event_name][index]
+                del self.blocking_priorities[event_name][index]
+
+    async def dispatch(self, event_name: str, *args, **kwargs) -> None:
+        """
+        Trigger dispatch of an event, passing args directly to each handler.
+
+        Beware passing mutable args--previous handlers, if misbehaving, can mutate them.
+        """
+        if event_name not in self.blocking_handlers:
+            logger.exception(
+                "Unregistered event '%s' was dispatched to no handlers with data: %r %r",
+                event_name,
+                args,
+                kwargs,
+            )
+            self.blocking_handlers[event_name] = []
+            self.handlers[event_name] = []
+            self.blocking_priorities[event_name] = []
+
+        for handler in self.blocking_handlers[event_name]:
+            if await handler(*args, **kwargs):
+                return
+
+        await asyncio.gather(*(handler(*args, **kwargs) for handler in self.handlers[event_name]))
---
+++ tests/modmail/utils/test_dispatcher.py
@@ -0,0 +1,151 @@
+import pytest
+
+from modmail.utils.dispatcher import Dispatcher
+
+
+@pytest.fixture
+def dispatcher() -> Dispatcher:
+    """Ensure we can create a dispatcher."""
+    return Dispatcher("member_leave")
+
+
+def call_counter():
+    """Generates a function that returns the number of times it was called."""
+    tocks = 0
+
+    def counter():
+        nonlocal tocks
+        tocks += 1
+        return tocks
+
+    return counter
+
+
+def make_mock_handler():
+    """Generates an async function that returns the number of times it was called."""
+    tocks = 0
+
+    async def mock_handler(*args):
+        nonlocal tocks
+        tocks += 1
+        return tocks
+
+    return mock_handler
+
+
+@pytest.mark.dependency(name="register_thread_events")
+def test_register_thread_create_event(dispatcher: Dispatcher):
+    """Ensure registering events functions at all."""
+    dispatcher.register_event("thread_create", "thread_close", "thread_message")
+
+
+@pytest.mark.dependency(depends_on=["register_thread_events"])
+def test_register_function_straight(dispatcher: Dispatcher):
+    """Test the regular form of handler registration."""
+    handler = make_mock_handler()
+
+    dispatcher.register("thread_create", handler)
+
+
+@pytest.mark.dependency(depends_on=["register_thread_events"])
+@pytest.mark.asyncio
+async def test_register_function_decorator(dispatcher: Dispatcher):
+    """Test the decorator form of handler registration."""
+    calls = 0
+
+    @dispatcher.register("thread_create")
+    async def mock_handler(*args):
+        nonlocal calls
+        calls += 1
+
+    await dispatcher.dispatch("thread_create", False)
+
+    assert calls == 1
+
+
+@pytest.mark.dependency(depends_on=["register_thread_events"])
+@pytest.mark.asyncio
+async def test_register_function_deconame(dispatcher: Dispatcher):
+    """Test that basic dispatch works."""
+    calls = 0
+
+    @dispatcher.register()
+    async def on_thread_create(*args):
+        nonlocal calls
+        calls += 1
+
+    await dispatcher.dispatch("thread_create", False)
+
+    assert calls == 1
+
+
+@pytest.mark.dependency(depends_on=["register_thread_events"])
+@pytest.mark.asyncio
+async def test_register_unregister(dispatcher: Dispatcher):
+    """Test that unregistering prevents a handler from being called."""
+    calls = 0
+
+    @dispatcher.register()
+    async def on_thread_create(*args):
+        nonlocal calls
+        calls += 1
+
+    await dispatcher.dispatch("thread_create", False)
+
+    assert calls == 1
+
+    dispatcher.unregister(on_thread_create)
+
+    await dispatcher.dispatch("thread_create", True)
+
+    assert calls == 1
+
+
+@pytest.mark.dependency(depends_on=["register_thread_events"])
+@pytest.mark.asyncio
+async def test_priority_order(dispatcher: Dispatcher):
+    """Test priority ordering and blocking of further event dispatch works.."""
+    calls = 0
+
+    @dispatcher.register()
+    async def on_thread_create(*args):
+        nonlocal calls
+        calls += 1
+
+    priority_calls = 0
+
+    @dispatcher.register("thread_create", priority=3)
+    async def priority_handler(*args):
+        nonlocal priority_calls
+        priority_calls += 1
+        return True
+
+    await dispatcher.dispatch("thread_create", False)
+
+    assert priority_calls == 1
+    assert calls == 0
+
+    high_priority_calls = 0
+
+    @dispatcher.register("thread_create", priority=1)
+    async def high_priority_handler(*args):
+        nonlocal high_priority_calls
+        high_priority_calls += 1
+        return False
+
+    await dispatcher.dispatch("thread_create", False)
+
+    assert priority_calls == 2
+    assert calls == 0
+    assert high_priority_calls == 1
+
+
+@pytest.mark.dependency(depends_on=["register_thread_events"])
+@pytest.mark.asyncio
+async def test_bad_name_raises(dispatcher: Dispatcher):
+    """Test that attempting to register a function without a clear event name fails."""
+    with pytest.raises(ValueError):
+
+        @dispatcher.register()
+        async def bad_name(*args):
+            pass
