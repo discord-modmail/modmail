716e1213c3e9ceeb0b4780d70d22fd1b51a5a823
onerandomusername <genericusername414+git@gmail.com>
2021-09-23 04:45:23 -0400

    add some CheckFailure handling support

--- modmail/extensions/utils/error_handler.py
+++ modmail/extensions/utils/error_handler.py
@@ -1,4 +1,5 @@
 import logging
+import re
 import typing
 
 import discord
@@ -17,12 +18,23 @@ EXT_METADATA = ExtMetadata()
 ERROR_COLOUR = discord.Colour.red()
 
 
+def _raise(e: BaseException, *arg, **kwargs) -> typing.NoReturn:
+    raise e
+
+
 class ErrorHandler(ModmailCog, name="Error Handler"):
     """Handles all errors across the bot."""
 
     def __init__(self, bot: ModmailBot):
         self.bot = bot
 
+    @commands.check(lambda _: _raise(commands.NotOwner("oy")))
+    @commands.command()
+    async def error(self, ctx: commands.Context) -> None:
+        """."""
+        await ctx.send(5)
+
+    @commands.has_permissions(administrator=False)
     @commands.command()
     async def cmd(self, ctx: commands.Context, error: str = None, *args) -> None:
         """."""
@@ -41,22 +53,17 @@ class ErrorHandler(ModmailCog, name="Error Handler"):
     ) -> discord.Embed:
         """Handling deferred from main error handler to handle UserInputErrors."""
         embed = None
-        if isinstance(error, commands.BadArgument):
-            embed = self.error_embed(str(error), title="Bad argument")
-        elif isinstance(error, commands.MissingRequiredArgument):
-            embed = self.error_embed(str(error), title="Missing required argument")
-        elif isinstance(error, commands.TooManyArguments):
-            embed = self.error_embed(str(error), title="Too many arguments")
-        elif isinstance(error, commands.BadUnionArgument):
+        title = "User Input Error"
+        if isinstance(error, commands.BadUnionArgument):
             # TODO: complete
             msg = ""
-            embed = self.error_embed(msg, title="Bad union argument")
+            embed = self.error_embed(msg, title="Bad Union Argument")
         elif isinstance(error, commands.BadLiteralArgument):
             # TODO: complete
             msg = ""
-            embed = self.error_embed(msg, title="Bad literal argument")
+            embed = self.error_embed(msg, title="Bad Literal Argument")
         elif isinstance(error, commands.ArgumentParsingError):
-            embed = self.error_embed(str(error), title="Argument parsing error")
+            embed = self.error_embed(str(error), title="Argument Parsing Error")
             if isinstance(error, commands.UnexpectedQuoteError):
                 ...
             elif isinstance(error, commands.InvalidEndOfQuotedStringError):
@@ -66,12 +73,27 @@ class ErrorHandler(ModmailCog, name="Error Handler"):
             else:
                 ...
         else:
-            ...
-        return embed
+            title = re.sub(r"([A-Z])", r" \1", error.__class__.__name__)
+        return embed or self.error_embed(str(error), title=title)
 
-    async def handle_check_failure(self, ctx: commands.Context, error: commands.CheckFailure) -> ...:
+    async def handle_check_failure(
+        self, ctx: commands.Context, error: commands.CheckFailure
+    ) -> discord.Embed:
         """Handle CheckFailures seperately given that there are many of them."""
-        ...
+        title = "Check Failure"
+        embed = None
+        if isinstance(error, commands.CheckAnyFailure):
+            ...
+        elif isinstance(error, commands.PrivateMessageOnly):
+            title = "DMs Only"
+        elif isinstance(error, commands.NoPrivateMessage):
+            title = "Server Only"
+        elif isinstance(error, commands.NSFWChannelRequired):
+            title = "NSFW Channel Required"
+        else:
+            title = re.sub(r"([A-Z])", r" \1", error.__class__.__name__)
+        embed = self.error_embed(str(error), title=title)
+        return embed
 
     @ModmailCog.listener()
     async def on_command_error(self, ctx: commands.Context, error: commands.CommandError) -> None:
@@ -95,7 +117,7 @@ class ErrorHandler(ModmailCog, name="Error Handler"):
         if isinstance(error, commands.UserInputError):
             embed = await self.handle_user_input_error(ctx, error)
         elif isinstance(error, commands.CheckFailure):
-            ...
+            embed = await self.handle_check_failure(ctx, error)
         elif isinstance(error, commands.ConversionError):
             s = object()
             error.converter.convert.__annotations__.get("return", s)
