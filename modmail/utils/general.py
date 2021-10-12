b06c7fc8cc2695821440a95969b05ec63af60e0e
Bast <bast@fastmail.com>
2021-10-11 23:02:03 -0700

    Add utility function to help distinguish functions lingering from reloaded cogs

--- modmail/utils/general.py
+++ modmail/utils/general.py
@@ -36,3 +36,29 @@ def await_nonblocking(func: Coroutine) -> asyncio.Task:
     task.add_done_callback(ensure_exception)
 
     return task
+
+
+def module_function_disidenticality(func1: Callable, func2: Callable) -> bool:
+    """
+    Determine if two functions are probably from the same module loaded twice.
+
+    Returns true if:
+    - same name
+    - same qualname
+    - same module path
+    - but not the same function object
+
+    This happens when a module is reloaded and old references are kept around.
+    We unfortunately cannot compare the module object itself, as I don't know a
+    way to access it from a function.
+
+    This can also happen if someone is generating functions in a loop and not
+    setting __name__ or __qualname__, but it's better practice to do that to
+    name those appropriately.
+    """
+    return (
+        func1.__qualname__ == func2.__qualname__
+        and func1.__name__ == func2.__name__
+        and func1.__module__ == func2.__module__
+        and func1 != func2
+    )
