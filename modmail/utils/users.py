d90a36a617fb47b8674dafc47e2e0e91321b5093
onerandomusername <genericusername414+git@gmail.com>
2021-10-18 11:59:30 -0400

    nit: better detail 100% error rate on check_can_dm_user

--- modmail/utils/users.py
+++ modmail/utils/users.py
@@ -2,7 +2,12 @@ import discord
 
 
 async def check_can_dm_user(user: discord.User) -> bool:
-    """Checks if the user has a DM open."""
+    """
+    Checks if the user has a DM open.
+
+    NOTE: This method has a 100% error rate, so it should not be used unless absolutely necessary.
+    Repeated usage can lead to a discord api ban.
+    """
     try:
         await user.send("")
     except discord.errors.Forbidden:
