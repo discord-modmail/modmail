7c9055c9cd5e5f988693c5ca89fef3fda9d96056
onerandomusername <genericusername414+git@gmail.com>
2021-11-16 06:38:12 -0500

    logging: add config for trace loggers and logging directory
    - allow configuring loggers and their specified levels
    
    Some loggers can get spammy when debugging, this allows
    turning them down, or setting other loggers to lower levels than the root
    
    To use, set a MODMAIL_{level}_LOGGERS environment variable, delimiting
    the loggers with `,`
    Valid levels are all of the valid levels for logging, as follows
      trace, debug, info, notice, warning, error, critical
    
    - add support for configuring the file logging directory
    
    The directory for logging files was fully dependent on the current
    working directory
    This caused my environment to be littered with logging directories
    
    The solution to this was to continue to use the current working
    directory, unless the parent directory of the bot is also a parent of
    the cwd, in which case the bot parent directory is used for creating
    the logging directory.
    
    In addition, `MODMAIL_LOGGING_DIRECTORY` has been added as an override
    environment variable. Setting it will use that directory for logging.

--- modmail/log.py
+++ modmail/log.py
@@ -1,5 +1,103 @@
 import logging
+import pathlib
 from typing import Any
+
+
+__all__ = [
+    "DEFAULT",
+    "get_logging_level",
+    "set_logger_levels",
+    "ModmailLogger",
+]
+
+logging.TRACE = 5
+logging.NOTICE = 25
+logging.addLevelName(logging.TRACE, "TRACE")
+logging.addLevelName(logging.NOTICE, "NOTICE")
+
+DEFAULT = logging.INFO
+
+
+def _get_env() -> dict:
+    import os
+
+    try:
+        from dotenv import dotenv_values
+    except ModuleNotFoundError:
+        dotenv_values = lambda *args, **kwargs: dict()  # noqa: E731
+
+    return {**dotenv_values(), **os.environ}
+
+
+def get_logging_level() -> None:
+    """Get the configured logging level, defaulting to logging.INFO."""
+    key = "MODMAIL_LOG_LEVEL"
+
+    level = _get_env().get(key, DEFAULT)
+
+    try:
+        level = int(level)
+    except TypeError:
+        level = DEFAULT
+    except ValueError:
+        level = level.upper()
+        if hasattr(logging, level) and isinstance(getattr(logging, level), int):
+            return getattr(logging, level)
+        print(
+            f"Environment variable {key} must be able to be converted into an integer.\n"
+            f"To resolve this issue, set {key} to an integer value, or remove it from the environment.\n"
+            "It is also possible that it is sourced from an .env file."
+        )
+        exit(1)
+
+    return level
+
+
+def set_logger_levels() -> None:
+    """
+    Set all loggers to the provided environment variables.
+
+    eg MODMAIL_TRACE_LOGGERS will be split by `,` and each logger will be set to the trace level
+    This is applied for every logging level.
+    """
+    env_vars = _get_env()
+    fmt_key = "MODMAIL_{level}_LOGGERS"
+
+    for level in ["trace", "debug", "info", "notice", "warning", "error", "critical"]:
+        level = level.upper()
+        key = fmt_key.format(level=level)
+        loggers: str = env_vars.get(key, None)
+        if loggers is None:
+            continue
+
+        for logger in loggers.split(","):
+            logging.getLogger(logger.strip()).setLevel(level)
+
+
+def get_log_dir() -> pathlib.Path:
+    """
+    Return a directory to be used for logging.
+
+    The log directory is made in the current directory
+    unless the current directory shares a parent directory with the bot.
+
+    This is ignored if a environment variable provides the logging directory.
+    """
+    env_vars = _get_env()
+    key = "MODMAIL_LOGGING_DIRECTORY"
+    if env_vars.get(key, None) is not None:
+        return pathlib.Path(env_vars[key]).expanduser()
+
+    import modmail
+
+    path = pathlib.Path(modmail.__file__).parent.parent
+    cwd = pathlib.Path.cwd()
+    try:
+        cwd.relative_to(path)
+    except ValueError:
+        return cwd / "logs"
+    else:
+        return path / "logs"
 
 
 class ModmailLogger(logging.Logger):
