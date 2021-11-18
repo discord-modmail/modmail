8be233298773132d6963be44d542b61078971711
onerandomusername <genericusername414+git@gmail.com>
2021-11-18 04:11:25 -0500

    chore: fix windows dns lookup

--- tests/modmail/addons/test_utils.py
+++ tests/modmail/addons/test_utils.py
@@ -9,25 +9,12 @@ from modmail.addons.models import AddonSource, SourceTypeEnum
 from modmail.addons.utils import download_zip_from_source
 
 
-# https://github.com/discord-modmail/modmail/archive/main.zip
-
-
-@pytest.fixture()
-@pytest.mark.asyncio
-async def session() -> ClientSession:
-    """Fixture function for a aiohttp.ClientSession."""
-    sess = ClientSession()
-    yield sess
-
-    await sess.close()
-
-
 @pytest.mark.parametrize(
     "source", [AddonSource.from_zip("https://github.com/discord-modmail/modmail/archive/main.zip")]
 )
 @pytest.mark.asyncio
-async def test_download_zip_from_source(source: AddonSource, session: ClientSession):
+async def test_download_zip_from_source(source: AddonSource, http_session: ClientSession):
     """Test that a zip can be successfully downloaded and everything is safe inside."""
-    file = await download_zip_from_source(source, session)
+    file = await download_zip_from_source(source, http_session)
     assert isinstance(file, zipfile.ZipFile)
     assert file.testzip() is None
