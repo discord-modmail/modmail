43bfa34f55e8a00413017238370d66064dd01b15
onerandomusername <genericusername414+git@gmail.com>
2021-11-19 01:22:35 -0500

    tests: add aioresponses for aiohttp response mocking

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,26 @@
 import aiohttp
+import aioresponses
 import pytest
 
 
 @pytest.fixture
+def aioresponse():
+    """Fixture to mock aiohttp responses."""
+    with aioresponses.aioresponses() as aioresponse:
+        yield aioresponse
+
+
+@pytest.fixture
 @pytest.mark.asyncio
-async def http_session() -> aiohttp.ClientSession:
-    """Fixture function for a aiohttp.ClientSession."""
+async def http_session(aioresponse) -> aiohttp.ClientSession:
+    """
+    Fixture function for a aiohttp.ClientSession.
+
+    Requests fixture aioresponse to ensure that all client sessions do not make actual requests.
+    """
     resolver = aiohttp.AsyncResolver()
     connector = aiohttp.TCPConnector(resolver=resolver)
-    client_session = aiohttp.ClientSession(connector=connector)
+    client_session = aiohttp.ClientSession()
 
     yield client_session
 
